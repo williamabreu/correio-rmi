8064e9991212fc449939a32106da65001082adf0
williamabreu <williamsantos_00@hotmail.com>
2018-10-21 15:45:07 -0300

    Adicionado métodos a Message
    - Cria uma nova mensagem passando parâmetros por string;
    - Imprime mensagem formatada.

--- src/lcd/mail/Message.java
+++ src/lcd/mail/Message.java
@@ -1,13 +1,36 @@
-package mail;
+package lcd.mail;
 
-import java.util.Date;
 import java.io.Serializable;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Message implements Serializable {
+
+    private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
 
     private String usernameFrom;
     private String title;
     private String text;
     private Date date;
+
+    public Message(String usernameFrom, String date, String title, String text) throws ParseException {
+        this.usernameFrom = usernameFrom;
+        this.title = title;
+        this.text = text;
+        this.date = dateFormatter.parse(date);
+    }
+
+    @Override
+    public String toString() {
+        String out = "";
+
+        out += "De:     " + usernameFrom               + "\n";
+        out += "Data:   " + dateFormatter.format(date) + "\n";
+        out += "Título: " + title                      + "\n";
+        out += "Texto:  " + text;
+
+        return out;
+    }
+
 }
