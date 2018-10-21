2cf849231a8a39bcb525522b8ad1c78d89d17335
williamabreu <williamsantos_00@hotmail.com>
2018-10-21 14:51:35 -0300

    Adicionado métodos a User
    - Impossível mudar username e senha;
    - Senha hash seguro;
    - Lista de mensagens somente leitura.

--- src/lcd/mail/User.java
+++ src/lcd/mail/User.java
@@ -1,12 +1,37 @@
-package mail;
+package lcd.mail;
 
-import java.util.List;
+import lcd.security.SecurePassword;
+
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class User implements Serializable {
 
     private String username;
-    private String password;
+    private String passwordHash;
+    private byte[] passwordSalt;
     private List<Message> messageList;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.passwordSalt = SecurePassword.genSalt();
+        this.passwordHash = SecurePassword.genSecurePassword(password, passwordSalt);
+        this.messageList = new LinkedList();
+    }
+
+    public List<Message> getMessages() {
+        return Collections.unmodifiableList(messageList);
+    }
+
+    public boolean checkUsername(String username) {
+        return this.username.equals(username);
+    }
+
+    public boolean checkPassword(String password) {
+        String passwordHash = SecurePassword.genSecurePassword(password, passwordSalt);
+        return this.passwordHash.equals(passwordHash);
+    }
 
 }
