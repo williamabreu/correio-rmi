c6fd0abef79a5742ee774e9794149fcacfdd4915
williamabreu <williamsantos_00@hotmail.com>
2018-10-21 21:22:50 -0300

    Verificação de parâmetros nos sh scripts

--- client.sh
+++ client.sh
@@ -1,2 +1,7 @@
-cd ./out/production/correio-rmi/
-java -Dfile.encoding=UTF-8 -classpath . lcd.main.Main client $1 $2
+if [ -z $1 ] || [ -z $2 ];
+then
+    echo "Parâmetros: <host> <port>"
+else
+    cd ./out/production/correio-rmi/
+    java -Dfile.encoding=UTF-8 -classpath . lcd.main.Main client $1 $2
+fi
--- server.sh
+++ server.sh
@@ -1,3 +1,8 @@
-cd ./out/production/correio-rmi/
-rmiregistry &
-java -Dfile.encoding=UTF-8 -classpath . lcd.main.Main server $1 $2
+if [ -z $1 ] || [ -z $2 ];
+then
+    echo "Parâmetros: <host> <port>";
+else
+    cd ./out/production/correio-rmi/
+    rmiregistry &
+    java -Dfile.encoding=UTF-8 -classpath . lcd.main.Main server $1 $2
+fi
